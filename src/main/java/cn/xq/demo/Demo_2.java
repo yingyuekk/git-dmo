f98ce6871f5d4a60c8448640c36a0e773c8a522c
yingyuekk <1308056152@qq.com>
2021-06-16 23:10:46 +0800

    git 练习 -----本地分支dev-2上的首次提交

---
+++ src/main/java/cn/xq/demo/Demo_2.java
@@ -0,0 +1,15 @@
+package cn.xq.demo;
+
+/**
+ * <p> Demo_2 </p>
+ * 在本地分支dev-2上创建的类
+ * @author Joan
+ * @version 1.0
+ * @date 2021/6/16 23:09
+ */
+public class Demo_2 {
+
+    public static void main(String[] args) {
+        System.out.println("在本地分支dev-2上创建的类");
+    }
+}
